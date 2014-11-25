48295787f6889113002cbadaaf15c605d1e06e8b
daveywilkie <davey.wilkie@gmail.com>
2014-11-24 16:22:30 -1000

    changes for resubmission

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 frontend-nanodegree-arcade-game
 ===============================
 
-To run the application, simply open the 'index.html' file in any browser that runs JavaScript.
+You will need to downloaded the application to a local directory, simply by clicking [this link](https://github.com/daveywilkie/frontend-nanodegree-arcade-game/archive/master.zip) and unzipping the file.
+
+To run the application, simply open the 'index.html' file in any Internet browser that runs JavaScript. For most users, this is as simple as double-clicking the index file, otherwise by right-clicking the index and selecting a browser with which to open it.
+
+Please not that the app will not work if you try to open the index file directly from GitHub, which will instead present the file's source code in a viewer hosted on their site.
+
--- js/app.js
+++ js/app.js
@@ -1,18 +1,41 @@
-// Enemies our player must avoid
+/**
+ * @fileoverview This is the core application file for a clone of the classic
+ * arcade game, Frogger.
+ * @author David Wilkie
+ * @since 24 November, 2014
+ * @copyright Copyright (c) 2014, David Wilkie
+ */
+
+/**
+ * Enemies our player must avoid.
+ * @constructor
+ */ 
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    /** 
+     * The path to an image/sprite for our enemies. 
+     * @type {string}
+     */
     this.sprite = 'images/enemy-bug.png';
+    /**
+     * X coordinate.
+     * @type {number}
+     */
     this.x = 0;
+    /**
+     * Y coordinate.
+     * @type {number}
+     */
     this.y = 0;
+    /** 
+     * Randomly set enemy speed.
+     * @type {number}
+     */
     this.speed = Math.floor(Math.random() * 250) + 100;
-}
+};
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+/** Updates the enemy's position, required method for game.
+ * @parameter {number} dt This is a time delta between ticks.
+ */
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
@@ -21,50 +44,84 @@ Enemy.prototype.update = function(dt) {
     if (this.x > 600) { 
         this.reset()
     }
-}
+};
 
-// Draw the enemy on the screen, required method for game
+/** 
+ * Draws the enemy on the screen, required method for game.
+ */
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
-// Move the enemy back to the beginning of the path and randomize its speed
+/** 
+ * Moves the enemy back to the beginning of the path and randomize its speed.
+ */
 Enemy.prototype.reset = function() {
     this.x = -50;
     this.y = 83 * (Math.floor(Math.random() * 3)) + 60;
     this.speed = Math.floor(Math.random() * 250) + 100;
-}
+};
 
+/**
+ * Returns coordinates of this enemy.
+ *
+ * @return {array} The x y coords.
+ */
 Enemy.prototype.getCoords = function() {
     coords = [this.x, this.y];
     return coords;
-}
+};
 
-// Player class
+/** 
+ * Player class.
+ * @constructor
+ */
 var Player = function() {
+    /** 
+     * The path to an image/sprite for our player. 
+     * @type {string}
+     */
     this.sprite = 'images/char-boy.png';
-    this.x = 202; 
-    this.y = 332; 
-}
+    /**
+     * X coordinate.
+     * @type {number}
+     */
+    this.x = 0;
+    /**
+     * Y coordinate.
+     * @type {number}
+     */
+};
 
-// Upon reaching the other side, move player to start (with a reset)
+/** 
+ * Upon reaching the other side, moves player to start (with a reset).
+ * @parameter {number} dt This is a time delta between ticks.
+ */
 Player.prototype.update = function(dt) {
     if (this.y < 83) {
         this.reset();
     }
-}
+};
 
+/**
+ * Makes a call to the ctx class to render the player.
+ */
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
-// Move the player to the starting position
+/** 
+ * Moves the player to the starting position.
+ */
 Player.prototype.reset = function() {
     this.x = 202;
     this.y = 332;
-}
+};
 
-// Controls player movement
+/**
+ * Controls player movement.
+ * @parameter {number} keyCode The ASCII number of the key pressed.
+ */
 Player.prototype.handleInput= function(keyCode) {
     if (keyCode == 'left') { 
         if (player.x >= 101) {
@@ -86,22 +143,11 @@ Player.prototype.handleInput= function(keyCode) {
             player.y += 83;
         } 
     }
-}
+};
 
-// Instantiates objects.
-// Places all enemy objects in an array called allEnemies
-// Places the player object in a variable called player
-player = new Player;
-allEnemies = [];
-for (var i=0;i<6;i++) {
-    e = new Enemy;
-    e.x = Math.random() * 450;
-    e.y = 83 * (Math.floor(Math.random() * 3)) + 60;
-    allEnemies.push(e);
-}
-
-// This listens for key presses and sends the keys to the
-// Player.handleInput() method.
+/** Listens for key presses and sends the keys to the Player.handleInput()
+ *  method.
+ */ 
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -112,3 +158,16 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+/** 
+ * Instantiates objects, places all enemy objects in an array called allEnemies,
+ * and places the player object in a variable called player
+ */
+player = new Player;
+allEnemies = [];
+for (var i=0;i<6;i++) {
+    e = new Enemy;
+    e.x = Math.random() * 450;
+    e.y = 83 * (Math.floor(Math.random() * 3)) + 60;
+    allEnemies.push(e);
+};
